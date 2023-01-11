61f339a6dad8f722f93bcebea0f74f21f7b60212
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2023-01-11 13:39:54 -0800

    Revisiting this project, but will probably not complete today. Added a bit more code and narrowing down the method

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,39 +1,58 @@
-
-var searchFormEl = document.getElementById("search-form");
-
-function searchWeather(event) {
-  event.preventDefault();
-
-  var searchInputVal = document.querySelector('#search-input').value;
-
-
-
-
+let searchButton = document.getElementById("search-button");
+let location = document.getElementById("location");
+let currentDate = document.getElementById("current-date");
+let icon = document.getElementById("icon");
+let temp = document.getElementById("temp");
+let weatherDetails = document.getElementById("weather-details");
+let humidity = document.getElementById("humidity");
+let windSpeed = document.getElementById("wind-speed");
+let uvIndex = document.getElementById("uv-index");
 
 
-searchFormEl.addEventListener('submit', getWeather);
 
 
-var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=63a94d459d52f9c7cb3b910e98b67749';
 
+
+function getCity() {
+  const newInput=document.getElementById("search-input")
+  
+
+  
+
+
+
+
+
+
+searchFormEl.addEventListener('submit', getCity);
+
+
+
+}
 
 
 function getWeather() {
-
+  let requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=seattle&appid=63a94d459d52f9c7cb3b910e98b67749&units=imperial';
 fetch(requestUrl)
   .then(function (response) {
-    return response.json();
-  })
+    if (response.ok) { 
+    return response.json()
+  
   .then(function (data) {
+    console.log(data);
 
-icon.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
-
-temp.innerHTML = `${weather.temperature.value} ° <span>F</span>`;
-
-descrtt.innerHTML = weather.description;
-
-location.innerHTML = `${weather.city}, ${weather.country}`;
 })
-console.log("button click");
+
 };
+})
 }
+
+
+// icon.innerHTML = `<img src="icons/${weather.icon}.png"/>`;
+
+// temp.innerHTML = `${weather.temperature.value} ° <span>F</span>`;
+
+// description.innerHTML = weather.description;
+
+// location.innerHTML = `${weather.city}, ${weather.country}`;
+getWeather();
